2c24d04e8f7f1bfa8132ab18eeae4076c5bdf38f
MPRA-1985 <55998575+MPRA-1985@users.noreply.github.com>
2021-04-21 22:03:12 -0300

    Add files via upload
    ATIVIDADE 2 - Implementar na calculadora criada no exercicio anterior outras operações.
    
    Implementar na calculadora criada as operações de exponenciação, raiz quadrada, 1/x e % como na calculadora do windows.
    Atualizar o github e postar o link na atividade.
    A correção do exercicio anterior está no link abaixo.

--- calc.php
+++ calc.php
@@ -4,21 +4,48 @@
 	$op = $_GET["operacao"];
 
 	if( $op == "soma" ) {
+
 		$z = $x + $y;
+
 	} elseif ( $op == "subtracao" ) {
+
 		$z = $x - $y;
+
 	} elseif ( $op == "multiplicacao" ) {
+
 		$z = $x * $y;
-	} else {
+
+	} elseif ( $op == "divisao" ) {
 		
 		if ( $y != 0 ) {
+
 			$z = ($x / $y);
+
 		} else {
+
 			echo "Não é possível dividir um número por 0.";
 			return 0;
-		}
-	} 
 
-	echo "O resultado e: $z";
+		}
+
+	} elseif ( $op == "potenciacao" ) {
+
+		$z = pow($x, $y);
+
+	} elseif ( $op == "raizQuadrada" ) {
+
+		$z = sqrt($x);
+
+	} elseif ( $op == "umSobreX" ) {
+
+		$z = (1 / $x);
+
+	} elseif ( $op == "porcentagem" ) {
+
+		$z = ($x / 100 * $y);
+
+	}
+
+	echo "<script>alert('Resultado: $z'); location= './calculadora.html';</script>";
 
 ?>
